b3a607fac194b2ba4a87b741e7dffce5f73051b8
shibuya takumi <takm.reason@gmail.com>
2025-11-05 01:58:00 +0900

    Amplify SSR対応およびビルド設定修正

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'export',
-  distDir: 'out',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
 };
 
 export default nextConfig;
